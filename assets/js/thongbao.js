db44dc0a4e36f52eeb8b5e5e70c8ce17b590d343
Thangdv007 <thangdv007@gmail.com>
2023-04-11 15:35:14 +0700

    blank

---
+++ assets/js/thongbao.js
@@ -0,0 +1,10 @@
+﻿function submitForm(){
+    document.getElementById('myform').submit();
+	alert("Yêu cầu đặt phòng của bạn đã được gửi. Chúng tôi sẽ gọi lại hoặc gửi Email để xác nhận việc đặt chỗ của bạn. Cảm ơn bạn!");
+}
+function submitForm1(){
+    document.getElementById('myform1').submit();
+	alert("Tin nhắn của bạn đã được gửi. Cảm ơn bạn!");
+}
+
+	
